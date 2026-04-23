e7fcded9ad2ee241414eb187415e705223a36a57
Shashank Raj <shashankweb111@gmail.com>
2026-04-23 23:56:27 +0530

    initial new

--- src/features/interview/hooks/useInterview.js
+++ src/features/interview/hooks/useInterview.js
@@ -74,17 +74,20 @@ export const useInterview = () => {
 
   const getResumePdf = async (interviewReportId) => {
     setLoading(true);
-    let response = null;
+
     try {
-      response = await generateResumePdf({ interviewReportId });
-      const url = window.URL.createObjectURL(
-        new Blob([response], { type: "application/pdf" }),
-      );
+      const response = await generateResumePdf({ interviewReportId });
+
+      const url = window.URL.createObjectURL(response);
+
       const link = document.createElement("a");
       link.href = url;
-      link.setAttribute("download", `resume_${interviewReportId}.pdf`);
+      link.download = `resume_${interviewReportId}.pdf`;
+
       document.body.appendChild(link);
       link.click();
+
+      window.URL.revokeObjectURL(url);
     } catch (error) {
       console.log(error);
     } finally {

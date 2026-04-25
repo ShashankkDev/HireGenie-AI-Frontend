ec68ccbfa8ad0faf9618b63ac65e9668f2f4ac2c
Shashank Raj <shashankweb111@gmail.com>
2026-04-25 16:08:42 +0530

    fix routing properly

--- src/features/auth/components/Protected.jsx
+++ src/features/auth/components/Protected.jsx
@@ -1,20 +1,25 @@
 import { useAuth } from "../hooks/useAuth";
 import { Navigate } from "react-router";
-import React from 'react'
+import React from "react";
 
-const Protected = ({children}) => {
-    const { loading,user } = useAuth()
+const Protected = ({ children }) => {
+  const { loading, user } = useAuth();
 
+  if (loading) {
+    return (
+      <main className="loading-screen">
+        <div className="loading-glow" />
+        <div className="loader"></div>
+        <h1 className="loading-text">Preparing your AI experience...</h1>
+      </main>
+    );
+  }
 
-    if(loading){
-        return (<main><h1>Loading...</h1></main>)
-    }
+  if (!user) {
+    return <Navigate to={"/login"} />;
+  }
 
-    if(!user){
-        return <Navigate to={'/login'} />
-    }
-    
-    return children
-}
+  return children;
+};
 
-export default Protected
+export default Protected;
